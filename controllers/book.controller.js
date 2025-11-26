a2711956581e6edea7da2dd347b1a1c1c4973a06
Putro Satrio Wibowo <putro.wibowo@kemenkeu.go.id>
2025-11-26 15:11:23 +0700

    Fix: query logic 1. endpoint Get Book By Id 2. endpoint Create Book 3. endpoint Delete Book By Id

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,20 +1,18 @@
 const { books } = require('../models/book');
 const db = require('../db');
 const { booksTable } = require('../models');
+const { eq } = require('drizzle-orm');
 
 exports.getAllBooks = async (req, res) => {
     const books = await db.select().from(booksTable);
 	return res.json(books);
 }
 
-exports.getBookById = (req, res) => {
-    const Id = parseInt(req.params.id);
-    if(isNaN(Id)){
-        return res.status(400).json({
-            error: 'ID must be number'
-        });
-    }
-    const book = books.find(b => b.id === Id);
+exports.getBookById = async (req, res) => {
+    const Id = req.params.id;
+
+    // const book = await books.find(b => b.id === Id);
+    const [book] = await db.select().from(booksTable).where(eq(booksTable.id, Id)).limit(1);
     if(!book){
         return res.status(404).json({ 
             error: `Book with ID ${Id} does not exist`
@@ -23,8 +21,8 @@ exports.getBookById = (req, res) => {
     return res.json(book);
 }
 
-exports.createBook = (req, res) => {
-    const { title, author } = req.body;
+exports.createBook = async (req, res) => {
+    const { title, authorId, description } = req.body;
 
     if (!title || title.trim() === '') {
         return res.status(400).json({
@@ -32,41 +30,26 @@ exports.createBook = (req, res) => {
         });
     }
 
-    if (!author || author.trim() === '') {
+    if (!authorId || authorId.trim() === '') {
         return res.status(400).json({
-        error: 'Author is required'
+            error: 'Author ID is required'
         });
     }
 
-    const id = books.length + 1;
-    const book = { id, title, author };
+    const book = { title, authorId, description };
 
-    books.push(book);
+    const [result] = await db.insert(booksTable).values(book).returning({ id: booksTable.id});
 
     return res.status(201).json({
         message: 'Book created successfully',
-        id: id
+        id: result.id
     });
 }
 
-exports.deleteBook = (req, res) => {
-    const id = parseInt(req.params.id);
+exports.deleteBook = async (req, res) => {
+    const id = req.params.id;
 
-    if (isNaN(id)) {
-        return res.status(400).json({
-        error: 'ID must be of type number'
-        });
-    }
-
-    const indexToDelete = books.findIndex(b => b.id === id);
-
-    if (indexToDelete < 0) {
-        return res.status(404).json({
-            error: `Book with id ${id} does not exist`
-        });
-    }
-
-    books.splice(indexToDelete, 1);
+    await db.delete(booksTable).where(eq(booksTable.id, id));
 
     return res.status(200).json({
         message: 'Book deleted'
